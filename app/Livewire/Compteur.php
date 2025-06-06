4b275bb1bab3725f6fdd184c7bac26463f78e8e7
PyrusHoungbeme <houngbemepyrus@gmail.com>
2025-06-06 18:58:36 +0100

    Ajout delivewire

---
+++ app/Livewire/Compteur.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Compteur extends Component
+{
+    public function render()
+    {
+        return view('livewire.compteur');
+    }
+}
---
+++ app/Livewire/MonComposantLiveWire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MonComposantLiveWire extends Component
+{
+    public function render()
+    {
+        return view('livewire.mon-composant-live-wire');
+    }
+}
